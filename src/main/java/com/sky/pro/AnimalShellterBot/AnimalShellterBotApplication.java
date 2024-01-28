dacbcd66d4b9f42bb9a33839dbad98bb76aa3639
internal-syntax <sandro-wold@live.ru>
2024-01-28 18:57:21 +0500

    Initial commit

---
+++ src/main/java/com/sky/pro/AnimalShellterBot/AnimalShellterBotApplication.java
@@ -0,0 +1,13 @@
+package com.sky.pro.AnimalShellterBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShellterBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalShellterBotApplication.class, args);
+	}
+
+}
